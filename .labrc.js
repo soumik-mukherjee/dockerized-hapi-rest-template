8cb225c50a3c8d1064a87cd423cd37b0bafd7916
Soumik Mukherjee <me@soumikmukherjee.dev>
2020-07-01 18:26:43 +0530

    fix: upload coverage report with release

---
+++ .labrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    coverage: true,
+    threshold: 0,
+    reporter: ['console', 'html'],
+    output: ['stdout', 'coverage.html'],
+    'coverage-exclude':[
+        'babel.config.js',
+        'es6-lab.config.js'
+    ]
+};
