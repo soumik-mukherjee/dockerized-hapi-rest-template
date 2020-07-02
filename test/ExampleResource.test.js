e226f961b729c3573d4191f1af136a180746a9df
Soumik Mukherjee <me@soumikmukherjee.dev>
2020-07-02 18:37:14 +0530

    fix: same test

--- test/ExampleResource.test.js
+++ test/ExampleResource.test.js
@@ -20,7 +20,7 @@ describe("SonnyTheHumanoid", () => {
     const { res, payload } = response;
     expect(res.statusCode).to.equal(201);
         });
-it("responds in plain text", () => {const { res, payload } = response;expect(res.headers["content-type"]).to.equal("text/plain; charset=utf-8");});
+ it("responds in plain text", () => {const { res, payload } = response;expect(res.headers["content-type"]).to.equal("text/plain; charset=utf-8");});
 
   it("thanks creator", async () => {
     const { res, payload } = response;
