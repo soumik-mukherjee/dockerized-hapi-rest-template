ee854dd4b8b83aa03738b09d536922c808390030
Soumik Mukherjee <me@soumikmukherjee.dev>
2020-07-03 00:05:30 +0530

    fix: testing prettier multi dir

--- test/ExampleResource.test.js
+++ test/ExampleResource.test.js
@@ -22,7 +22,8 @@ describe("SonnyTheHumanoid", () => {
   });
   it("responds in plain text", () => {
     const { res, payload } = response;
-expect(res.headers["content-type"]).to.equal("text/plain; charset=utf-8");
+
+    expect(res.headers["content-type"]).to.equal("text/plain; charset=utf-8");
   });
 
   it("thanks creator", async () => {
