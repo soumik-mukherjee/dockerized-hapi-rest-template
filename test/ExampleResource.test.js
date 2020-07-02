3202d3194ba60cce513c177c2e71ee85e8fa23bd
Soumik Mukherjee <me@soumikmukherjee.dev>
2020-07-02 18:40:48 +0530

    fix: prettifier fix 'test' folder

--- test/ExampleResource.test.js
+++ test/ExampleResource.test.js
@@ -19,7 +19,7 @@ describe("SonnyTheHumanoid", () => {
   it("can be created", () => {
     const { res, payload } = response;
     expect(res.statusCode).to.equal(201);
-        });
+         });
  it("responds in plain text", () => {const { res, payload } = response;expect(res.headers["content-type"]).to.equal("text/plain; charset=utf-8");});
 
   it("thanks creator", async () => {
